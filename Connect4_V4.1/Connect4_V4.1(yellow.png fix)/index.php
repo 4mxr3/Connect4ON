3dd3cafd873eb3fcba41026e4231f4a716b9a8b5
CatLoversOfAllCats <164790558+CatLoversOfAllCats@users.noreply.github.com>
2024-04-27 11:20:52 -0700

    Minor PNG fix for yellow dots

---
+++ Connect4_V4.1/Connect4_V4.1(yellow.png fix)/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
